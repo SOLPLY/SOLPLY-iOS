d98d187a217c59cf82a4847426fd7716ed512b47
Won <andrew112517@naver.com>
2025-06-27 05:30:15 +0900

    [feat/#1] hideKeyboard

---
+++ Solply/Solply/Global/Extension/View+.swift
@@ -0,0 +1,13 @@
+//
+//  View+.swift
+//  Solply
+//
+//  Created by 김승원 on 6/27/25.
+//
+
+import SwiftUI
+
+/// 현재 화면에서 키보드를 내립니다.
+func hideKeyboard() {
+    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+}
