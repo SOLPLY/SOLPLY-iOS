097db3586231edc09a3f1fac0b7f044699727473
Won <andrew112517@naver.com>
2025-06-27 05:31:12 +0900

    [feat/#1] CustomNavigationBarModifier

---
+++ Solply/Solply/Global/Util/CustomNavigationBarModifier.swift
@@ -0,0 +1,55 @@
+//
+//  CustomNavigationBarModifier.swift
+//  Solply
+//
+//  Created by 김승원 on 6/27/25.
+//
+
+import SwiftUI
+
+struct CustomNavigationBarModifier<C, L, R>: ViewModifier where C: View, L: View, R: View {
+    let centerView: (() -> C)?
+    let leftView: (() -> L)?
+    let rightView: (() -> R)?
+    let backgroundColor: Color
+    
+    init(
+        centerView: (() -> C)? = nil,
+        leftView: (() -> L)? = nil,
+        rightView: (() -> R)? = nil,
+        backgroundColor: Color = .clear
+    ) {
+        self.centerView = centerView
+        self.leftView = leftView
+        self.rightView = rightView
+        self.backgroundColor = backgroundColor
+    }
+    
+    func body(content: Content) -> some View {
+        VStack(spacing: 0) {
+            ZStack(alignment: .center) {
+                HStack(spacing: 0) {
+                    self.leftView?()
+                    
+                    Spacer()
+                    
+                    self.rightView?()
+                }
+                
+                self.centerView?()
+                
+            }
+            /*
+             Todo: 네비게이션 패딩값 나오면 수정 필요
+             */
+            .padding(.horizontal, 0)
+            .padding(.vertical, 0)
+            .background(backgroundColor)
+            
+            content
+            
+            Spacer()
+        }
+        .navigationBarHidden(true)
+    }
+}
