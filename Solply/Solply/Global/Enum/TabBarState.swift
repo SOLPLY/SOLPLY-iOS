94edd448228a7229ea17d97bfdb6840089e8aff4
Won <andrew112517@naver.com>
2025-06-27 17:54:58 +0900

    [feat/#2] TabBarState Enum 구현

---
+++ Solply/Solply/Global/Enum/TabBarState.swift
@@ -0,0 +1,20 @@
+//
+//  TabBarState.swift
+//  Solply
+//
+//  Created by 김승원 on 6/27/25.
+//
+
+import Foundation
+
+enum TabBarState: CaseIterable {
+    case place
+    case course
+    
+    var title: String {
+        switch self {
+        case .place: return "장소"
+        case .course: return "코스"
+        }
+    }
+}
