4b12cb4807b4c6ef568fe852879125317ca6d65f
Won <andrew112517@naver.com>
2025-06-27 18:43:51 +0900

    [feat/#2] MyPageButton 구현

---
+++ Solply/Solply/Presentation/Core/Component/MyPageButton.swift
@@ -0,0 +1,36 @@
+//
+//  MyPageButton.swift
+//  Solply
+//
+//  Created by 김승원 on 6/27/25.
+//
+
+import SwiftUI
+
+struct MyPageButton: View {
+    
+    // MARK: - Properties
+    
+    private let onTap: (() -> Void)?
+    
+    // MARK: - Initializer
+    
+    init(onTap: (() -> Void)?) {
+        self.onTap = onTap
+    }
+    
+    // MARK: - Body
+    
+    var body: some View {
+        Button {
+            onTap?()
+        } label: {
+            Image(systemName: "house.fill")
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 50.adjustedWidth, height: 50.adjustedHeight)
+                .background(Color(.systemGray5))
+                .circleClipped()
+        }
+        .buttonStyle(.plain)
+    }
+}
