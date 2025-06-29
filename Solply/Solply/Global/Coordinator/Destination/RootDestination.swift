05a71857e3cbea12a8725b31778b1a00cd65a6cf
Won <andrew112517@naver.com>
2025-06-29 17:49:19 +0900

    [feat/#3] Destination 추가

---
+++ Solply/Solply/Global/Coordinator/Destination/RootDestination.swift
@@ -0,0 +1,14 @@
+//
+//  RootDestination.swift
+//  Solply
+//
+//  Created by 김승원 on 6/28/25.
+//
+
+import Foundation
+
+enum RootDestination {
+    case auth
+    case onboarding
+    case tabBar
+}
