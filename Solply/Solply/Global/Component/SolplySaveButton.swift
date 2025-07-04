7675cfa4f0ec83af89a2578d08580d655f81060e
Won <andrew112517@naver.com>
2025-07-04 22:29:34 +0900

    [feat/#17] 컴포넌트 구현

---
+++ Solply/Solply/Global/Component/SolplySaveButton.swift
@@ -0,0 +1,60 @@
+//
+//  SolplySaveButton.swift
+//  Solply
+//
+//  Created by 김승원 on 7/4/25.
+//
+
+import SwiftUI
+
+struct SolplySaveButton: View {
+    
+    // MARK: - Properties
+    
+    private let contentType: SolplyContentType
+    private var isEnabled: Bool
+    private var isSelected: Bool
+    private let action: (() -> Void)?
+
+    // MARK: - Initializer
+    
+    init(
+        contentType: SolplyContentType,
+        isEnabled: Bool,
+        isSelected: Bool,
+        action: (() -> Void)? = nil
+    ) {
+        self.contentType = contentType
+        self.isEnabled = isEnabled
+        self.isSelected = isSelected
+        self.action = action
+    }
+    
+    // MARK: - Body
+    
+    var body: some View {
+        Button {
+            action?()
+        } label: {
+            HStack(alignment: .center, spacing: 8.adjustedWidth) {
+                Text(isSelected ? "저장된 \(contentType.title)" : "\(contentType.title) 저장")
+                    .applySolplyFont(.button_14_m)
+                    .foregroundStyle(isEnabled ? (isSelected ? .red500: .purple600) : .gray400)
+                
+                Image(.tempIcon)
+                    .resizable()
+                    .renderingMode(.template)
+                    .frame(width: 23.adjustedWidth, height: 23.adjustedHeight)
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundStyle(isEnabled ? (isSelected ? .red500 : .purple600) : .gray400)
+            }
+            .frame(width: isSelected ? 126.adjustedWidth : 114.adjustedWidth, height: 47.adjustedHeight,)
+            .background(isEnabled ? (isSelected ? .red100 : .coreWhite) : .coreWhite)
+            .capsuleClipped()
+            .animation(.easeInOut(duration: 0.2), value: isEnabled)
+            .animation(.easeInOut(duration: 0.2), value: isSelected)
+        }
+        .allowsHitTesting(isEnabled)
+        .buttonStyle(.plain)
+    }
+}
