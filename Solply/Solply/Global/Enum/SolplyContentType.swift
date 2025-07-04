f2d14ed299f6633460c9b39ce8d964ce96826ddf
Won <andrew112517@naver.com>
2025-07-04 22:28:46 +0900

    [feat/#17] SolplyContentType 추가 (장소, 코스)

---
+++ Solply/Solply/Global/Enum/SolplyContentType.swift
@@ -0,0 +1,20 @@
+//
+//  SolplyContentType.swift
+//  Solply
+//
+//  Created by 김승원 on 7/4/25.
+//
+
+import Foundation
+
+enum SolplyContentType {
+    case place
+    case course
+    
+    var title: String {
+        switch self {
+        case .place: return "장소"
+        case .course: return "코스"
+        }
+    }
+}
