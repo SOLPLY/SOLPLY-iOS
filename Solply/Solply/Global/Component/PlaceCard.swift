bcfecf000ad6c2bb008daa520272c7f2b9f8615f
pedro0527 <sooyong0527@naver.com>
2025-07-08 17:33:27 +0900

    [feat/#31] 장소 컴포넌트 카드 구현

---
+++ Solply/Solply/Global/Component/PlaceCard.swift
@@ -0,0 +1,47 @@
+//
+//  PlaceCard.swift
+//  Solply
+//
+//  Created by LEESOOYONG on 7/8/25.
+//
+
+import SwiftUI
+
+struct PlaceCard: View {
+    
+    // MARK: - Properties
+    
+    private let isSaved: Bool
+    private let title: String
+    private let placeCategory: PlaceCategoryType
+    
+    // MARK: - Initializer
+    
+    init(isSaved: Bool, title: String, placeCategory: PlaceCategoryType) {
+        self.isSaved = isSaved
+        self.title = title
+        self.placeCategory = placeCategory
+    }
+    
+    // MARK: - Body
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8.adjustedHeight) {
+            ZStack(alignment: .bottomTrailing) {
+                Image(.place)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 165.adjustedWidth, height: 165.adjustedHeight)
+                (isSaved ? Image(placeCategory.savedBadge ?? "") : nil)
+                    .aspectRatio(contentMode: .fit)
+                    .padding(.horizontal, 12.adjustedWidth)
+                    .padding(.vertical, 12.adjustedHeight)
+            }
+            HStack(alignment: .center, spacing: 4.adjustedWidth) {
+                PlaceCategoryTag(placeCategory: placeCategory)
+                Text(title)
+                    .applySolplyFont(.body_14_m)
+            }
+        }
+    }
+}
