304e685fb6178b404c4b39ccde286a91a303ffbc
pedro0527 <sooyong0527@naver.com>
2025-07-09 17:59:48 +0900

    [feat/#49] ArchiveAction 구현

---
+++ Solply/Solply/Presentation/Archive/ArchiveIntent/ArchiveAction.swift
@@ -0,0 +1,12 @@
+//
+//  ArchiveAction.swift
+//  Solply
+//
+//  Created by LEESOOYONG on 7/9/25.
+//
+
+import Foundation
+
+enum ArchiveAction {
+    case toggleArchiveBar(archiveCategory: SolplyContentType)
+}
