3273e3d300f81327a62420f60ba37b2c69e157e8
pedro0527 <sooyong0527@naver.com>
2025-07-09 18:00:33 +0900

    [feat/#49] ArchiveReducer 구현

---
+++ Solply/Solply/Presentation/Archive/ArchiveState/ArchiveReducer.swift
@@ -0,0 +1,17 @@
+//
+//  ArchiveReducer.swift
+//  Solply
+//
+//  Created by LEESOOYONG on 7/9/25.
+//
+
+import Foundation
+
+enum ArchiveReducer {
+    static func reduce(state: inout ArchiveState, action: ArchiveAction) {
+        switch action {
+        case .toggleArchiveBar(let archiveCategory):
+            state.selectedCategory = archiveCategory
+        }
+    }
+}
