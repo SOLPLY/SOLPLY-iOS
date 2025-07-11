d7455debfd7abb710ed31f801d3157036b733224
pedro0527 <sooyong0527@naver.com>
2025-07-12 05:47:16 +0900

    [feat/#60] 수집함 grid MVI 패턴 적용

---
+++ Solply/Solply/Presentation/Archive/ArchiveList/State/ArchiveListReducer.swift
@@ -0,0 +1,25 @@
+//
+//  ArchiveListReducer.swift
+//  Solply
+//
+//  Created by LEESOOYONG on 7/10/25.
+//
+
+import Foundation
+
+enum ArchiveListReducer {
+    static func reduce(state: inout ArchiveListState, action: ArchiveListAction) {
+        switch action {
+        case .toggleArchiveList(let index):
+            if state.selectedIndex.contains(index) {
+                state.selectedIndex.remove(index)
+            } else {
+                state.selectedIndex.insert(index)
+            }
+        case .toggleSelect:
+            state.activeDelete.toggle()
+        case .toggleCancel:
+            state.selectedIndex.removeAll()
+        }
+    }
+}
