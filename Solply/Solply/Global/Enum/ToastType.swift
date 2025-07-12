288f5ba5eecced7bf1583e238fdc07f7c0b16e5f
Won <andrew112517@naver.com>
2025-07-12 23:13:12 +0900

    [feat/#75] ToastType 구현

---
+++ Solply/Solply/Global/Enum/ToastType.swift
@@ -0,0 +1,14 @@
+//
+//  ToastType.swift
+//  Solply
+//
+//  Created by 김승원 on 7/12/25.
+//
+
+import SwiftUI
+
+enum ToastType {
+    case defaultToast
+    case withActionToast
+    case withAlertToast
+}
