acd4fd77334f31c05b6ac6b649ce20692c5fce8f
Won <andrew112517@naver.com>
2025-07-12 14:06:51 +0900

    [feat/#70] DeleteZomeState 구현

---
+++ Solply/Solply/Global/Enum/DeleteZoneState.swift
@@ -0,0 +1,15 @@
+//
+//  DragState.swift
+//  Solply
+//
+//  Created by 김승원 on 7/12/25.
+//
+
+import Foundation
+
+enum DeleteZoneState {
+    case hidden
+    case active
+    case dismissed
+}
+
