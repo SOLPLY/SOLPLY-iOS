b90b35d244401f5185864d6b69a5ffedbe3b3cdf
seozero <tjwndud0412@naver.com>
2025-07-15 06:33:02 +0900

    [feat/#110] FilterButton 액션 추가

--- Solply/Solply/Presentation/Place/Component/FilterButton.swift
+++ Solply/Solply/Presentation/Place/Component/FilterButton.swift
@@ -12,30 +12,36 @@ struct FilterButton: View {
     // MARK: - Properties
     
     private let title: String
+    private let action: (() -> Void)?
     
     // MARK: - Initializer
     
-    init(title: String) {
+    init(title: String, action: (() -> Void)? = nil) {
         self.title = title
+        self.action = action
     }
     
     // MARK: - Body
     
     var body: some View {
-        HStack(alignment: .center, spacing: 0) {
-            Text(title)
-                .applySolplyFont(.body_14_m)
-            Image(.arrowDownIcon)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 24.adjustedWidth)
+        Button {
+            action?()
+        } label: {
+            HStack(alignment: .center, spacing: 0) {
+                Text(title)
+                    .applySolplyFont(.body_14_m)
+                Image(.arrowDownIcon)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 24.adjustedWidth)
+            }
+            .foregroundStyle(.coreBlack)
+            .background(.coreWhite)
+            .padding(.vertical, 5.5.adjustedHeight)
+            .padding(.leading, 12.adjustedWidth)
+            .padding(.trailing, 4.adjustedWidth)
+            .capsuleClipped()
+            .addBorder(.capsule, borderColor: .gray300, borderWidth: 0.75)
         }
-        .foregroundStyle(.coreBlack)
-        .background(.coreWhite)
-        .padding(.vertical, 5.5.adjustedHeight)
-        .padding(.leading, 12.adjustedWidth)
-        .padding(.trailing, 4.adjustedWidth)
-        .capsuleClipped()
-        .addBorder(.capsule, borderColor: .gray300, borderWidth: 0.75)
     }
 }
