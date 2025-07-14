21700d65e1575028b1ee2174f2f3e007f3be414d
SunyoungJu <sts07190@naver.com>
2025-07-15 01:28:29 +0900

    [fix/#90] 코드 리뷰 반영

--- Solply/Solply/Presentation/Splash/LottieView.swift
+++ Solply/Solply/Presentation/Splash/LottieView.swift
@@ -10,11 +10,10 @@ import UIKit
 
 import Lottie
 
-
 struct LottieView: UIViewRepresentable {
     var name : String
     var loopMode: LottieLoopMode
-
+    
     init(jsonName: String = "", loopMode : LottieLoopMode = .loop){
         self.name = jsonName
         self.loopMode = loopMode
@@ -22,25 +21,25 @@ struct LottieView: UIViewRepresentable {
     
     func makeUIView(context: UIViewRepresentableContext<LottieView>) -> UIView {
         let view = UIView(frame: .zero)
-
+        
         let animationView = LottieAnimationView(name: name)
-            animationView.loopMode = loopMode
-            animationView.contentMode = .scaleAspectFit
-            animationView.play()
-            animationView.translatesAutoresizingMaskIntoConstraints = false
-  
-  
-      animationView.translatesAutoresizingMaskIntoConstraints = false
+        animationView.loopMode = loopMode
+        animationView.contentMode = .scaleAspectFit
+        animationView.play()
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        animationView.translatesAutoresizingMaskIntoConstraints = false
         
         view.addSubview(animationView)
         NSLayoutConstraint.activate([
             animationView.heightAnchor.constraint(equalTo: view.heightAnchor),
             animationView.widthAnchor.constraint(equalTo: view.widthAnchor)
         ])
-
+        
         return view
     }
-
+    
     func updateUIView(_ uiView: UIViewType, context: Context) {
     }
 }
