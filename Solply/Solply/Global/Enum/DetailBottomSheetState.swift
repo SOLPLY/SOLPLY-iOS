161f1009cb1c8c45d489b1c6dc4b6c94949b9e32
Won <andrew112517@naver.com>
2025-07-15 00:10:10 +0900

    [feat/#93] BottomSheet enum 수정

---
+++ Solply/Solply/Global/Enum/DetailBottomSheetState.swift
@@ -0,0 +1,22 @@
+//
+//  DetailBottomSheetState.swift
+//  Solply
+//
+//  Created by 김승원 on 7/14/25.
+//
+
+import Foundation
+
+enum DetailBottomSheetState {
+    case courseExpanded
+    case placeExpended
+    case collapsed
+    
+    var maxHeight: CGFloat {
+        switch self {
+        case .courseExpanded: return 429.adjustedHeight // 467 - 28 - 10
+        case .placeExpended: return 487.adjustedHeight // 525 - 28 - 10
+        case .collapsed: return 150.adjustedHeight
+        }
+    }
+}
