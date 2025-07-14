5370c968d4590980a6b40c1edb56baa22752377f
SunyoungJu <sts07190@naver.com>
2025-07-14 16:42:33 +0900

    [feat/#83] 어스뷰 구현

--- Solply/Solply/Presentation/Auth/AuthView.swift
+++ Solply/Solply/Presentation/Auth/AuthView.swift
@@ -12,17 +12,70 @@ struct AuthView: View {
     @EnvironmentObject private var appCoordinator: AppCoordinator
     
     var body: some View {
-        VStack {
-            Text("AuthView")
+        ZStack {
             
-            Button {
-                appCoordinator.changeRoot(to: .onboarding)
-            } label: {
-                Text("chageRoot to OnboardingView")
+            VStack {
+                Image(.loginGraphic)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 165.adjustedHeight)
+                    .frame(maxWidth: .infinity)
+                    .clipped()
+                    .padding(.bottom, 60.adjustedHeight)
+            }
+            
+            VStack(spacing: 0) {
+                VStack(alignment: .leading, spacing: 0) {
+                    Image(.logoFullVector)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 40.adjustedHeight, height: 60.35069.adjustedHeight)
+                    
+                    Image(.letteringVector)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 215.adjustedHeight)
+                    
+                    Text("혼자만의 시간을\n더 쉽게, 더 즐겁게!")
+                        .applySolplyFont(.display_20_sb)
+                        .foregroundColor(.gray800)
+                        .padding(.top, 8.adjustedHeight)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.top, 84.adjustedHeight)
+                .padding(.horizontal, 40.adjustedHeight)
+                
+                Spacer()
+                
+                Button {
+                    appCoordinator.changeRoot(to: .onboarding)
+                } label: {
+                    HStack(alignment: .center, spacing: 12) {
+                        Image(.kakaoTalkIcon)
+                            .resizable()
+                            .frame(width: 28, height: 28)
+                        
+                        Text("카카오로 시작하기")
+                            .applySolplyFont(.button_16_m)
+                            .foregroundColor(Color(red: 0.1, green: 0.1, blue: 0.1))
+                    }
+                    .padding(.horizontal, 16)
+                    .padding(.vertical, 12)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .background(Color(red: 1, green: 0.9, blue: 0))
+                    .cornerRadius(12)
+                    .addBorder(
+                        .roundedRectangle(cornerRadius: 12),
+                        borderColor: Color(red: 0.9, green: 0.9, blue: 0.9),
+                        borderWidth: 1
+                    )
+                    .padding(.horizontal, 24.adjustedHeight)
+                    .padding(.bottom, 100.adjustedHeight)
+                }
             }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(.cyan)
+        .background(.gray100)
     }
 }
 
