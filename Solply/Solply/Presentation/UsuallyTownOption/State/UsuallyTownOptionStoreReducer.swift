36a8010a4da5d0eb9707013d734708c5caaa4d46
SunyoungJu <sts07190@naver.com>
2025-07-15 06:42:46 +0900

    [feat/#102] MVI 구현

---
+++ Solply/Solply/Presentation/UsuallyTownOption/State/UsuallyTownOptionStoreReducer.swift
@@ -0,0 +1,18 @@
+//
+//  UsuallyTownOptionStoreReducer.swift
+//  Solply
+//
+//  Created by 선영주 on 7/15/25.
+//
+
+import Foundation
+
+struct UsuallyTownOptionReducer {
+    static func reduce(state: inout UsuallyTownOptionState, action: UsuallyTownOptionAction) {
+        switch action {
+        case .selectOption(let option):
+            state.selectedOption = option
+            state.isUserSelected = true
+        }
+    }
+}
