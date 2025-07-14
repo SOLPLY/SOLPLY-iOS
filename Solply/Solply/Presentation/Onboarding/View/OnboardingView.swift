44c27cbbe3e093e439f7bb3557bb82f4c31b0c82
SunyoungJu <sts07190@naver.com>
2025-07-15 03:17:06 +0900

    [feat/#105] 네비게이션바 분기 처리

--- Solply/Solply/Presentation/Onboarding/View/OnboardingView.swift
+++ Solply/Solply/Presentation/Onboarding/View/OnboardingView.swift
@@ -9,56 +9,69 @@ import SwiftUI
 
 struct OnboardingView: View {
     
-    // MARK: - Properties
-    
     @EnvironmentObject private var appCoordinator: AppCoordinator
     @StateObject private var store: OnboardingStore = OnboardingStore()
     
-    // MARK: - Body
-    
     var body: some View {
-        VStack(alignment: .leading, spacing: 0) {
-            
-            if !store.state.isOnboardingFinished {
-                ProgressBar(step: store.state.step)
-                    .animation(.easeInOut(duration: 0.3), value: store.state.step)
+        Group {
+            if store.state.step != .onboardingComplete {
+                VStack(alignment: .leading, spacing: 0) {
+                    
+                    if !store.state.isOnboardingFinished {
+                        ProgressBar(step: store.state.step)
+                            .animation(.easeInOut(duration: 0.3), value: store.state.step)
+                    }
+                    
+                    switch store.state.step {
+                    case .townOption:
+                        TownOptionView(store: store)
+                    case .personaOption:
+                        PersonaOptionView(store: store)
+                    case .nickName:
+                        NicknameView(store: store)
+                    case .onboardingComplete:
+                        OnboardingCompleteView(store: store)
+                    }
+                    
+                    Spacer()
+                }
+                .animation(.easeOut(duration: 0.2), value: store.state.step)
+                .padding(.horizontal, 20)
+                .background(.gray100)
+                .onChange(of: store.state.isLottieFinished) { _, newValue in
+                    appCoordinator.changeRoot(to: .tabBar)
+                }
+                .onTapGesture {
+                    self.hideKeyboard()
+                }
+                .customNavigationBar(.onboarding(backAction: {
+                    switch store.state.step {
+                    case .townOption:
+                        appCoordinator.changeRoot(to: .auth)
+                    case .personaOption, .nickName:
+                        store.dispatch(.goBack)
+                    case .onboardingComplete:
+                        break
+                    }
+                }))
+            } else {
+                VStack(alignment: .leading, spacing: 0) {
+                    OnboardingCompleteView(store: store)
+                    Spacer()
+                }
+                .padding(.horizontal, 20)
+                .background(.gray100)
+                .onTapGesture {
+                    self.hideKeyboard()
+                }
             }
-            
-            switch store.state.step {
-            case .townOption:
-                TownOptionView(store: store)
-            case .personaOption:
-                PersonaOptionView(store: store)
-            case .nickName:
-                NicknameView(store: store)
-            case .onboardingComplete:
-                OnboardingCompleteView(store: store)
-            }
-            
-            Spacer()
-        }
-        .animation(.easeOut(duration: 0.2), value: store.state.step)
-        .padding(.horizontal, 20)
-        .customNavigationBar(.onboarding(backAction: {
-            switch store.state.step {
-            case .townOption:
-                appCoordinator.changeRoot(to: .auth)
-            case .personaOption:
-                store.dispatch(.goBack)
-            case .nickName:
-                store.dispatch(.goBack)
-            case .onboardingComplete:
-                break
-            }
-        }))
-        .background(.gray100)
-        .onChange(of: store.state.isLottieFinished) { _, newValue in
-            appCoordinator.changeRoot(to: .tabBar)
-        }
-        .onTapGesture {
-            self.hideKeyboard()
         }
     }
+}
+
+#Preview {
+    OnboardingView()
+        .environmentObject(AppCoordinator())
 }
 
 #Preview {
