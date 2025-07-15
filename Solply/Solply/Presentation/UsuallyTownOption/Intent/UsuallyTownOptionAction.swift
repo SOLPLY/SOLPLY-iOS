5ba93d58247493448a1cb8ab2e15bd98b4c3515f
SunyoungJu <sts07190@naver.com>
2025-07-15 17:19:10 +0900

    [feat/#121] 유즐리타운옵션뷰 MVI 구현

---
+++ Solply/Solply/Presentation/UsuallyTownOption/Intent/UsuallyTownOptionAction.swift
@@ -0,0 +1,12 @@
+//
+//  UsuallyTownOptionAction.swift
+//  Solply
+//
+//  Created by 선영주 on 7/15/25.
+//
+
+import Foundation
+
+enum UsuallyTownOptionAction {
+    case selectOption(TownOptionType)
+}
---
+++ Solply/Solply/Presentation/UsuallyTownOption/State/UsuallyTownOptionState.swift
@@ -0,0 +1,13 @@
+//
+//  UsuallyTownOptionState.swift
+//  Solply
+//
+//  Created by 선영주 on 7/15/25.
+//
+
+import Foundation
+
+struct UsuallyTownOptionState {
+    var selectedOption: TownOptionType? = nil
+}
+
