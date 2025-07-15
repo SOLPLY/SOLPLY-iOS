c0c24c03e1c29dae3fd6fd72b856e5933d7df796
seozero <tjwndud0412@naver.com>
2025-07-15 18:33:40 +0900

    [feat/#110] 추가 옵션 BottomSheet Header 구현

---
+++ Solply/Solply/Presentation/Place/Component/OptionHeader.swift
@@ -0,0 +1,45 @@
+//
+//  OptionHeader.swift
+//  Solply
+//
+//  Created by seozero on 7/15/25.
+//
+
+import SwiftUI
+
+struct OptionHeader: View {
+    
+    @Binding var isPresented: Bool
+    
+    private let title: String
+    private let isButtonVisible: Bool
+    
+    init(
+        isPresented: Binding<Bool>,
+        title: String,
+        isButtonVisible: Bool
+    ) {
+        self._isPresented = isPresented
+        self.title = title
+        self.isButtonVisible = isButtonVisible
+    }
+    
+    var body: some View {
+        HStack(alignment: .center, spacing: 0) {
+            Text(title)
+                .applySolplyFont(.title_18_sb)
+            
+            Spacer()
+            
+            Button {
+                isPresented = false
+            } label: {
+                Image(.xIconSm)
+                    .resizable()
+                    .frame(width: 24.adjustedWidth, height: 24.adjustedHeight)
+                    .buttonStyle(.plain)
+            }
+            .visible(isButtonVisible ? true : false)
+        }
+    }
+}
