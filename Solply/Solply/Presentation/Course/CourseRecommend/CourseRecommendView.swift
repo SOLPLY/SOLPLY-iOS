28cfed5abbf8b656391c0b9a847d5687fd687438
seozero <tjwndud0412@naver.com>
2025-07-15 20:35:58 +0900

    [feat/#123] CourseRecommendView 구현

--- Solply/Solply/Presentation/Course/CourseRecommend/CourseRecommendView.swift
+++ Solply/Solply/Presentation/Course/CourseRecommend/CourseRecommendView.swift
@@ -9,29 +9,71 @@ import SwiftUI
 
 struct CourseRecommendView: View {
     
+    // MARK: - Properties
+    
     @EnvironmentObject var appCoordinator: AppCoordinator
+    //    @StateObject private var store = PlaceRecommendStore()
+    
+    // MARK: - Body
     
     var body: some View {
         ScrollView {
-            VStack {
-                Text("CourseRecommendView")
-                
-                Button {
-                    appCoordinator.navigate(to: .courseDetail(fromArchive: false))
-                } label: {
-                    Text("코스 추천 -> 코스 상세")
+            VStack(alignment: .center, spacing: 28.adjustedHeight) {
+                HStack(alignment: .center, spacing: 0) {
+                    Text("차분함을 좋아하는\n숭이숭이숭이숭이님을 위한 오늘의 코스")
+                        .applySolplyFont(.display_20_sb)
+                    Spacer()
                 }
+                .padding(.horizontal, 20.adjustedWidth)
                 
-                Button {
-                    appCoordinator.navigate(to: .courseDetail(fromArchive: true))
-                } label: {
-                    Text("수집함 -> 코스 상세")
+                CourseRecommendGrid()
+            }
+            .frame(maxWidth: .infinity)
+            .padding(.bottom, 112.adjustedHeight)
+        }
+        .background(.gray100)
+        .customNavigationBar(.recommend(
+            filterTitle: "연희동",
+            // TODO: - navigation 연결하기
+            filterAction: {
+                
+            },
+            settingAction: {
+                
+            }
+        ))
+    }
+}
+
+struct CourseRecommendGrid: View {
+    
+    // MARK: - Properties
+    
+    private let columns = [
+        GridItem(.fixed(165.adjustedWidth)),
+        GridItem(.fixed(165.adjustedWidth))
+    ]
+    private let placeCategory: PlaceCategoryType = .all
+    
+    // MARK: - Body
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 16.adjustedHeight) {
+            LazyVGrid(columns: columns, spacing: 13.adjustedHeight) {
+                ForEach(0..<10) { index in
+                    CourseCard(
+                        isSaved: true,
+                        title: "오감으로 수집하는 하루",
+                        courseCategory: [.book, .cafe],
+                        isSelected: true
+                    ) {
+                        print("ㅇㅇ")
+                    }
+                    .frame(width: 165.adjustedWidth, height: 165.adjustedHeight)
                 }
             }
-            .frame(height: 1000)
-            .frame(maxWidth: .infinity)
         }
-        .background(.green)
+        .padding(.horizontal, 20.adjustedWidth)
     }
 }
 
@@ -39,3 +81,4 @@ struct CourseRecommendView: View {
     CourseRecommendView()
         .environmentObject(AppCoordinator())
 }
+
