48533d42a7713d0b2eb5e8c3f8c559409e4ffa81
seozero <tjwndud0412@naver.com>
2025-07-17 03:07:35 +0900

    [refactor/#155] 주석을 MARK 주석으로 변경

--- Solply/Solply/Global/Enum/MoreOptionType.swift
+++ Solply/Solply/Global/Enum/MoreOptionType.swift
@@ -9,13 +9,13 @@ import Foundation
 
 enum MoreOptionType: String {
 
-    // ===== 카페 추천 옵션 1 =====
+    // MARK: - 카페 추천 옵션 1
     case coffeeDessert = "COFFEE_DESSERT"
     case work = "WORK"
     case reading = "READING"
     case healing = "HEALING"
 
-    // ===== 카페 추천 옵션 2 =====
+    // MARK: - 카페 추천 옵션 2
     case signatureMenu = "SIGNATURE_MENU"
     case moodInterior = "MOOD_INTERIOR"
     case sunlight = "SUNLIGHT"
@@ -23,7 +23,7 @@ enum MoreOptionType: String {
     case noTimeLimit = "NO_TIME_LIMIT"
     case barTable = "BAR_TABLE"
 
-    // ===== 음식 추천 옵션 1 =====
+    // MARK: - 음식 추천 옵션 1
     case koreanFood = "KOREAN_FOOD"
     case chineseFood = "CHINESE_FOOD"
     case japaneseFood = "JAPANESE_FOOD"
@@ -32,15 +32,15 @@ enum MoreOptionType: String {
     case bakery = "BAKERY"
     case asianFood = "ASIAN_FOOD"
 
-    // ===== 음식 추천 옵션 2 =====
+    // MARK: - 음식 추천 옵션 2
     case singleMenu = "SINGLE_MENU"
     case selfService = "SELF_SERVICE"
 
-    // ===== 이색공간 추천 옵션 =====
+    // MARK: - 이색공간 추천 옵션
     case art = "ART"
     case workshop = "WORKSHOP"
 
-    // ===== 쇼핑 추천 옵션 =====
+    // MARK: - 쇼핑 추천 옵션
     case lifestyleShop = "LIFESTYLE_SHOP"
     case vintageShop = "VINTAGE_SHOP"
     case popupMarket = "POPUP_MARKET"
