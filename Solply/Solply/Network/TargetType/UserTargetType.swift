ceeaf6be5dfc4f310231abef79e28c74154b9e4d
seozero <tjwndud0412@naver.com>
2025-07-17 05:16:42 +0900

    [feat/#157] UserTargetType 작성

--- Solply/Solply/Network/TargetType/UserTargetType.swift
+++ Solply/Solply/Network/TargetType/UserTargetType.swift
@@ -10,5 +10,33 @@ import Foundation
 import Moya
 
 enum UserTargetType {
+    case fetchUserInformation
+}
+
+extension UserTargetType: BaseTargetType {
+    var headerType: HTTPHeader {
+        return .accessToken
+    }
+    
+    var path: String {
+        switch self {
+        case .fetchUserInformation:
+            return "/users"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .fetchUserInformation: return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .fetchUserInformation:
+            return .requestPlain
+        }
+    }
+    
     
 }
