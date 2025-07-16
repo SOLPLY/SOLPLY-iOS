51814e26cb453f5f922c3938674556581781f3d7
pedro0527 <sooyong0527@naver.com>
2025-07-16 21:16:21 +0900

    [feat/#142] 장소 TargetType 생성

--- Solply/Solply/Network/TargetType/PlaceTargetType.swift
+++ Solply/Solply/Network/TargetType/PlaceTargetType.swift
@@ -10,5 +10,33 @@ import Foundation
 import Moya
 
 enum PlaceTargetType {
+    case fetchPlaceThumbnail
+}
+
+extension PlaceTargetType: BaseTargetType {
+    var headerType: HTTPHeader {
+        switch self {
+        case .fetchPlaceThumbnail: return .accessToken
+        }
+    }
     
+    var path: String {
+        switch self {
+        case .fetchPlaceThumbnail:
+            return "/places/bookmarks/folders/preview"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .fetchPlaceThumbnail: return .get
+        }
+    }
+    
+    var task: Moya.Task {
+        switch self {
+        case .fetchPlaceThumbnail:
+            return .requestPlain
+        }
+    }
 }
