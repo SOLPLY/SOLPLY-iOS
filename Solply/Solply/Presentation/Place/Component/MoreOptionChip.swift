3bb58dcb2a4b6dd9fd27f5b698040fa355a0be09
seozero <tjwndud0412@naver.com>
2025-07-16 18:28:30 +0900

    [feat/#138] MoreOptionChip 버튼 제외

---
+++ Solply/Solply/Presentation/Place/Component/MoreOptionChip.swift
@@ -0,0 +1,35 @@
+//
+//  MoreOptionChip.swift
+//  Solply
+//
+//  Created by seozero on 7/15/25.
+//
+
+import SwiftUI
+
+struct MoreOptionChip: View {
+    
+    private let title: String
+    private var isSelected: Bool
+    
+    init(title: String, isSelected: Bool) {
+        self.title = title
+        self.isSelected = isSelected
+    }
+    
+    var body: some View {
+        Text(title)
+            .padding(.horizontal, 16.adjustedWidth)
+            .padding(.vertical, 8.adjustedHeight)
+            .applySolplyFont(.body_16_m)
+            .foregroundStyle(isSelected ? .coreWhite : .gray900)
+            .background(isSelected ? .gray900 : .coreWhite)
+            .capsuleClipped()
+            .addBorder(
+                .capsule,
+                borderColor: isSelected ? .clear : .gray300,
+                borderWidth: 0.75
+            )
+
+    }
+}
