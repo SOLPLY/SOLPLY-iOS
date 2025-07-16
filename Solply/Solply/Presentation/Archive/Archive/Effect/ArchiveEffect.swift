d6a0d43b7d732cf63143bf72ed9418003ddaffef
pedro0527 <sooyong0527@naver.com>
2025-07-16 21:17:51 +0900

    [feat/#142] 수집함 Effect 생성

---
+++ Solply/Solply/Presentation/Archive/Archive/Effect/ArchiveEffect.swift
@@ -0,0 +1,31 @@
+//
+//  ArchiveEffect.swift
+//  Solply
+//
+//  Created by LEESOOYONG on 7/16/25.
+//
+
+import Foundation
+
+struct ArchiveEffect {
+    private let service = PlaceService()
+    
+    func fetchPlaceThumbnail() async -> ArchiveAction {
+        do {
+            let response = try await service.fetchPlaceThumbnail()
+            
+            guard let data = response.data else {
+                return .errorOccured(error: .responseError)
+            }
+            
+            print(data)
+            
+            return .placeThumbnailFetched
+            
+        } catch let error as NetworkError {
+            return .errorOccured(error: error)
+        } catch {
+            return .errorOccured(error: .unknownError)
+        }
+    }
+}
