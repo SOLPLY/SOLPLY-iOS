31cc1e9b6cceff078e72b19754e4ffc0eadfc275
SunyoungJu <sts07190@naver.com>
2025-07-17 23:09:00 +0900

    [feat/#146] UserResponseDTO 구현

---
+++ Solply/Solply/Network/DTO/User/ResponseDTO/UserResponseDTO.swift
@@ -0,0 +1,12 @@
+//
+//  UserResponseDTO.swift
+//  Solply
+//
+//  Created by 선영주 on 7/17/25.
+//
+
+import Foundation
+
+struct CheckNicknameResponseDTO: Decodable, ResponseModelType {
+    let isDuplicated: Bool
+}
