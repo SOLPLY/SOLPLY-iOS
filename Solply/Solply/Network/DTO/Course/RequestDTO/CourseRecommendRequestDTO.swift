b54586be2d5dbd61d58ec605f852ab7499edbc05
seozero <tjwndud0412@naver.com>
2025-07-17 18:00:45 +0900

    [feat/#173] ResponseDTO, RequestDTO 작성

---
+++ Solply/Solply/Network/DTO/Course/RequestDTO/CourseRecommendRequestDTO.swift
@@ -0,0 +1,12 @@
+//
+//  CourseRecommendRequestDTO.swift
+//  Solply
+//
+//  Created by seozero on 7/17/25.
+//
+
+import Foundation
+
+struct CourseRecommendRequestDTO: RequestModelType {
+    let townId: Int
+}
