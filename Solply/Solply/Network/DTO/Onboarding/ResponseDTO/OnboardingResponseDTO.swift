5bf32d35cb0c6cd590f25c35934c7d9bc97403ff
SunyoungJu <sts07190@naver.com>
2025-07-17 23:03:39 +0900

    [feat/#146] OnboardingResponseDTO 구현

---
+++ Solply/Solply/Network/DTO/Onboarding/ResponseDTO/OnboardingResponseDTO.swift
@@ -0,0 +1,43 @@
+//
+//  OnboardingResponseDTO.swift
+//  Solply
+//
+//  Created by 선영주 on 7/17/25.
+//
+
+import Foundation
+
+// MARK: - Response DTO
+
+struct OnboardingResponseDTO: Decodable, ResponseModelType {
+    let personaList: [PersonaDTO]
+    let completeInfo: OnboardingCompleteResponseDTO?
+}
+
+struct PersonaDTO: Decodable, Hashable {
+    let personaType: String
+    let description: String
+
+    func toEntity() -> Persona {
+        return Persona(
+            type: personaType,
+            description: description
+        )
+    }
+}
+
+struct OnboardingCompleteResponseDTO: Decodable, ResponseModelType {
+    let selectedTownId: Int
+    let selectedTownName: String
+    let persona: String
+    let nickname: String
+
+    func toEntity() -> OnboardingCompleteInfo {
+        return OnboardingCompleteInfo(
+            townId: selectedTownId,
+            townName: selectedTownName,
+            persona: persona,
+            nickname: nickname
+        )
+    }
+}
