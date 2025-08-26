c06c2764fe773c8f27137c374caa3c17f0378734
dudwntjs <sts07190@naver.com>
2025-08-26 18:30:47 +0900

    [fix/#217] UserAPI 응답 DTO 구조 분리

---
+++ Solply/Solply/Network/DTO/User/ResponseDTO/UserTownsResponseDTO.swift
@@ -0,0 +1,13 @@
+//
+//  UserTownsResponseDTO.swift
+//  Solply
+//
+//  Created by sun on 8/26/25.
+//
+
+import Foundation
+
+struct UserTownsResponseDTO: ResponseModelType {
+    let selectedTown: TownDTO
+    let favoriteTownList: [TownDTO]
+}
