ceab3af5d22bd6c73aa159411fdba5c772602df1
dudwntjs <sts07190@naver.com>
2025-09-11 01:14:00 +0900

    [feat/#244] 토큰 재발급 API 구현

--- Solply/Solply/Network/TargetType/AuthTargetType.swift
+++ Solply/Solply/Network/TargetType/AuthTargetType.swift
@@ -11,30 +11,35 @@ import Moya
 
 enum AuthTargetType {
     case submitLogin(provider: String, request: AuthLoginRequestDTO)
+    case refreshToken(refreshToken: String)
 }
 
 extension AuthTargetType: BaseTargetType {
     var headerType: HTTPHeader {
         switch self {
         case .submitLogin: return .contentTypeJSON
+        case .refreshToken(let refreshToken): return .refreshToken(refreshToken)
         }
     }
     
     var path: String {
         switch self {
         case .submitLogin(let provider, _): return "/auth/social/\(provider)/login"
+        case .refreshToken: return "/auth/refresh"
         }
     }
     
     var method: Moya.Method {
         switch self {
         case .submitLogin: return .post
+        case .refreshToken: return .post
         }
     }
     
     var task: Moya.Task {
         switch self {
         case .submitLogin(_, let request): return .requestJSONEncodable(request)
+        case .refreshToken: return .requestPlain
         }
     }
 }
