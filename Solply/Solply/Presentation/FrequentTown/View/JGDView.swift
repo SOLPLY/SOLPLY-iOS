c40a60a4b69fd4c56f766b837e3917d8e32784a7
seozero <tjwndud0412@naver.com>
2025-09-22 01:55:51 +0900

    [feat/#243] 자주 가는 동네 뷰 UI 구현

--- Solply/Solply/Presentation/FrequentTown/View/JGDView.swift
+++ Solply/Solply/Presentation/FrequentTown/View/JGDView.swift
@@ -8,16 +8,62 @@
 import SwiftUI
 
 struct JGDView: View {
+    
+    @EnvironmentObject private var appCoordinator: AppCoordinator
+    @ObservedObject var store = FrequentTownStore()
+    
+    @State private var selectedCity: String? = "서울"
+    @State private var selectedTown: String? = nil
+    
+    // TODO: - API 연결해보기
+    var cityList: [String] = ["서울", "경기", "인천", "부산", "대구"]
+    var townList: [String] = ["망원동", "연희동", "성수동", "한남동"]
+    
     var body: some View {
-        HStack(alignment: .center, spacing: 0) {
+        ZStack(alignment: .bottom) {
+            VStack(alignment: .center, spacing: 0) {
+                Rectangle()
+                    .frame(height: 1)
+                    .foregroundStyle(.gray300)
+                HStack(alignment: .top, spacing: 0) {
+                    VStack(alignment: .center, spacing: 0) {
+                        VStack(alignment: .center, spacing: 0) {
+                            ForEach(cityList, id: \.self) { city in
+                                JGDCityRow(
+                                    selectedCity: $selectedCity,
+                                    selectedTown: $selectedTown,
+                                    city: city
+                                )
+                            }
+                        }
+                        
+                        Spacer()
+                    }
+                    .frame(maxHeight: .infinity)
+                    .background(.gray100)
+                    
+                    Divider()
+                    
+                    VStack(alignment: .center, spacing: 0) {
+                        ForEach(townList, id: \.self) { town in
+                            JGDTownRow(selectedTown: $selectedTown, town: town)
+                        }
+                    }
+                    .frame(width: 247.adjustedWidth)
+                }
+            }
+            .customNavigationBar(
+                .frequentTown(backAction: appCoordinator.goBack)
+            )
             
+            CTAMainButton(title: "완료")
+                .padding(.horizontal, 20)
+                .padding(.bottom, 16)
         }
-        .customNavigationBar(.frequentTown(backAction: {
-            
-        }))
     }
 }
 
 #Preview {
     JGDView()
+        .environmentObject(AppCoordinator())
 }
