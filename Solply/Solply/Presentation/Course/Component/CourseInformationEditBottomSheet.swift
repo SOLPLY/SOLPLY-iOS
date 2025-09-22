1ee9fb928ed4a887252446281f3b54e9f4814968
Won <andrew112517@naver.com>
2025-09-22 17:12:55 +0900

    [feat/#259] 코스 제목, 한 줄 소개 수정 bottomSheet UI 구현

---
+++ Solply/Solply/Presentation/Course/Component/CourseInformationEditBottomSheet.swift
@@ -0,0 +1,118 @@
+//
+//  CourseInformationEditBottomSheet.swift
+//  Solply
+//
+//  Created by 김승원 on 9/22/25.
+//
+
+import SwiftUI
+
+struct CourseInformationEditBottomSheet: View {
+    
+    // MARK: - Properties
+    
+    @State private var courseName: String
+    @State private var courseDescription: String
+    
+    private var dismissAction: (() -> Void)?
+    private var completeAction: ((String, String) -> Void)?
+    
+    // MARK: - Initializer
+    
+    init(
+        courseName: String,
+        courseDescription: String,
+        dismissAction: (() -> Void)? = nil,
+        completeAction: ((String, String) -> Void)? = nil
+    ) {
+        self.courseName = courseName
+        self.courseDescription = courseDescription
+        self.dismissAction = dismissAction
+        self.completeAction = completeAction
+    }
+    
+    // MARK: - Body
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 36.adjustedWidth) {
+            navigationBar
+            
+            courseNameEdit
+            
+            courseDescriptionEdit
+            
+            Spacer()
+            
+            completeButton
+        }
+        .ignoresSafeArea(.keyboard)
+        .background(.coreWhite)
+        .onTapGesture {
+            hideKeyboard()
+        }
+    }
+}
+
+// MARK: - Subviews
+
+extension CourseInformationEditBottomSheet {
+    private var navigationBar: some View {
+        ZStack(alignment: .center) {
+            HStack(alignment: .center, spacing: 0) {
+                Spacer()
+                
+                Button {
+                    dismissAction?()
+                } label: {
+                    Image(.xIconLg)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 24.adjustedWidth, height: 24.adjustedHeight)
+                }
+                .buttonStyle(.plain)
+            }
+            
+            Text("코스 정보 수정")
+        }
+        .frame(width: 343.adjustedWidth, height: 24.adjustedHeight)
+        .padding(.top, 16.adjustedHeight)
+    }
+    
+    private var courseNameEdit: some View {
+        VStack(alignment: .center, spacing: 12.adjustedHeight) {
+            Text("코스 이름")
+                .applySolplyFont(.title_14_m)
+                .foregroundStyle(.gray900)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            CourseInformationEditTextField(text: $courseName)
+        }
+        .padding(.horizontal, 16.adjustedWidth)
+    }
+    
+    private var courseDescriptionEdit: some View {
+        VStack(alignment: .center, spacing: 12.adjustedHeight) {
+            Text("코스 한 줄 소개")
+                .applySolplyFont(.title_14_m)
+                .foregroundStyle(.gray900)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            CourseInformationEditTextField(text: $courseDescription)
+        }
+        .padding(.horizontal, 16.adjustedWidth)
+    }
+    
+    private var completeButton: some View {
+        CTAMainButton(title: "완료") {
+            completeAction?(courseName, courseDescription)
+        }
+        .frame(width: 335.adjustedWidth, height: 64.adjustedHeight)
+    }
+}
+
+#Preview {
+    CourseInformationEditBottomSheet(
+        courseName: "오감으로 수집할까말까 ",
+        courseDescription: "오감 육감 칠감 ㅋㅋㅋ"
+    )
+}
