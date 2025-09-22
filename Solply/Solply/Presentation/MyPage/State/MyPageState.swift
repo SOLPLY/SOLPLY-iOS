3853dbf3f3ef4f3aef3038229a217faa1f96f2cb
dudwntjs <sts07190@naver.com>
2025-09-22 23:37:09 +0900

    [feat/#257] MyPage MVI 패턴

---
+++ Solply/Solply/Presentation/MyPage/State/MyPageState.swift
@@ -0,0 +1,15 @@
+//
+//  MyPageState.swift
+//  Solply
+//
+//  Created by sun on 9/19/25.
+//
+
+import Foundation
+
+
+struct MyPageState {
+    var nickname: String = ""
+    var loginProvider: String = ""
+    var places: [Place] = []
+}
