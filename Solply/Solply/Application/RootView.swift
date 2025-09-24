974ed7bc18a48c1dab659992610bad7641548fa0
Won <andrew112517@naver.com>
2025-09-24 18:37:30 +0900

    [refactor/#251] Alert띄우는 위치 RootView로 수정

--- Solply/Solply/Application/RootView.swift
+++ Solply/Solply/Application/RootView.swift
@@ -10,13 +10,16 @@ import SwiftUI
 struct RootView: View {
     @StateObject private var appCoordinator = AppCoordinator()
     @StateObject private var toastManager = ToastManager()
+    @StateObject private var alertManager = AlertManager()
     
     var body: some View {
         NavigationStack(path: $appCoordinator.path) {
             appCoordinator.root.build()
                 .navigationDestination(for: AppDestination.self) { $0.build() }
         }
+        .customAlert(alertManager: alertManager)
         .toast(toastManager: toastManager)
+        .environmentObject(alertManager)
         .environmentObject(toastManager)
         .environmentObject(appCoordinator)
         .animation(.easeInOut(duration: 0.2), value: appCoordinator.root)
