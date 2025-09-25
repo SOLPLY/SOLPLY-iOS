2166e651d7642805a285660cca7d0e6dac1a5e46
Won <andrew112517@naver.com>
2025-09-25 19:50:24 +0900

    [feat/#256] ReportsCompleteView UI 구현

--- Solply/Solply/Presentation/Reports/Component/View/ReportsCompleteView.swift
+++ Solply/Solply/Presentation/Reports/Component/View/ReportsCompleteView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct ReportsCompleteView: View {
     var body: some View {
-        Text("완료!!")
+        VStack(alignment: .center, spacing: 39.adjustedHeight) {
+            LottieView(jsonName: "finishOnboarding")
+                .frame(width: 230.adjustedWidth, height: 230.adjustedHeight)
+                
+            Text("소중한 제보 감사합니다!")
+                .applySolplyFont(.display_20_sb)
+        }
+        .background(.coreWhite)
     }
 }
 
