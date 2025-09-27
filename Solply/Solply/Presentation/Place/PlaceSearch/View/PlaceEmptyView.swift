3ca5de608e1d79341f0dcde33b51078dc0cc94bb
pedro0527 <sooyong0527@naver.com>
2025-09-27 12:26:59 +0900

    [feat] 빈 장소 데이터 뷰 구현

---
+++ Solply/Solply/Presentation/Place/PlaceSearch/View/PlaceEmptyView.swift
@@ -0,0 +1,28 @@
+//
+//  PlaceEmptyView.swift
+//  Solply
+//
+//  Created by LEESOOYONG on 9/25/25.
+//
+
+import SwiftUI
+
+struct PlaceEmptyView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 17.5.adjustedHeight) {
+            Text("검색 결과가 없어요.\n직접 장소 등록을 솔플리에 요청해보세요.")
+            
+            HStack(alignment: .center, spacing: 0) {
+                Text("장소 등록하러 가기")
+                    .applySolplyFont(.button_16_m)
+                    .foregroundColor(.purple600)
+                    
+                Image(.arrowRightIconPurple)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 24.adjustedWidth, height: 10.adjustedHeight)
+            }
+        }
+        .padding(.leading, 20.adjustedWidth)
+    }
+}
