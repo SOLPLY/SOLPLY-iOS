7c40b20e30588af24b90db2d5e7c445fe66f9e2c
Won <andrew112517@naver.com>
2025-10-28 20:15:09 +0900

    [chore/#288] 주석 제거

--- Solply/Solply/Presentation/Reports/Intent/ReportsStore.swift
+++ Solply/Solply/Presentation/Reports/Intent/ReportsStore.swift
@@ -23,9 +23,6 @@ final class ReportsStore: ObservableObject {
         switch action {
             
         case .changeReportsStep(let reportsStep):
-            // 1. 제보스탭 3단계 돌입(complete)
-            // 사용자가 제보 정보들을 입력하고 호출하는 단계
-            // -> reducer - submitPresignedUrlRequest 호출
             if let selectedReportsType = state.selectedReportsType, reportsStep == .reportsComplete {
                 if state.attachedImageData.isEmpty {
                     dispatch(
@@ -52,19 +49,12 @@ final class ReportsStore: ObservableObject {
             }
             
         case .submitPresignedUrlRequest(let request):
-            // 2. effect - submitPresignedUrlRequest 요청
-            // PresignedUrl을 발급받는 단계
-            // -> 성공) presignedUrlReqeustSubmitted()
-            // -> 실패) errorOccured
-            // reducer - presignedUrlReqeustSubmitted 호출
             Task {
                 let result = await effect.submitPresignedUrlRequest(request: request)
                 self.dispatch(result)
             }
             
         case .presignedUrlReqeustSubmitted(let response):
-            // 3. effect - reponse를 바탕으로 effect -  uploadImages 호출
-            // 사진데이터를 S3 업로드하는 과정
             let presignedInformation = response.presignedGetUrlInfos
             let imageDatas = state.attachedImageData
 
@@ -76,16 +66,12 @@ final class ReportsStore: ObservableObject {
                 }
             }
             
-            // UploadPhotosService 내부적으로 딕셔너리를 for문으로 돌려서 하나씩 S3에 업로드
-            // 성공시 .photoUploadSuccess 호출
             Task {
                 let result = await effect.uploadImages(dictionary: presignedDictionary)
                 self.dispatch(result)
             }
             
         case .photoUploadSuccess(let imageKeys):
-            // 4. S3 업로드 response를 가지고 DTO 생성
-            // effect - submitReports호출
             guard let reportsType = state.selectedReportsType, state.placeId > 0 else {
                 return
             }
@@ -105,7 +91,6 @@ final class ReportsStore: ObservableObject {
             self.dispatch(.submitReports(placeId: state.placeId, request: request))
             
         case .submitReports(let placeId, let request):
-            // 5. 제보하기하기 API 호출하는 단계
             Task {
                 let result = await effect.submitReports(placeId: placeId, request: request)
                 self.dispatch(result)
