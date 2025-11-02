489bf217858443287ad98b65b96b256e916aee84
pedro0527 <sooyong0527@naver.com>
2025-11-02 21:39:16 +0900

    [fix/#309] 스크롤 제거

--- Solply/Solply/Presentation/Place/PlaceSearch/Component/PlaceDataView.swift
+++ Solply/Solply/Presentation/Place/PlaceSearch/Component/PlaceDataView.swift
@@ -30,43 +30,40 @@ struct PlaceDataView: View {
     // MARK: - Body
     
     var body: some View {
-        ScrollView {
-            LazyVStack(alignment: .leading, spacing: 0) {
-
-                Text("검색 결과 \(places.count)개")
-                    .applySolplyFont(.button_14_m)
-                    .foregroundColor(.gray800)
-                    .frame(height: 18.adjustedHeight)
-                    .padding(.horizontal, 20.adjustedWidth)
-                    .padding(.bottom, 16.adjustedHeight)
-
-                ForEach(places.prefix(3), id: \.placeId) { place in
-                    SearchPlaceCard(
-                        thumbnailUrl: place.thumbnailImageUrl,
-                        placeName: place.placeName,
-                        address: place.address,
-                        mainTag: place.primaryTag,
-                    )
-                    .onTapGesture {
-                        placeDetailAction?(place.townId, place.placeId)
-                    }
-                }
-
-                HStack(alignment: .center, spacing: 0) {
-                    Text("찾는 장소가 없어요")
-                        .applySolplyFont(.button_14_m)
-                        .foregroundColor(.gray700)
-                        .padding(.leading, 20.adjustedWidth)
-
-                    Image(.arrowRightIconGray)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 24.adjustedWidth, height: 10.adjustedHeight)
-                }
-                .padding(.top, 16.adjustedHeight)
+        LazyVStack(alignment: .leading, spacing: 0) {
+            Text("검색 결과 \(places.count)개")
+                .applySolplyFont(.button_14_m)
+                .foregroundColor(.gray800)
+                .frame(height: 18.adjustedHeight)
+                .padding(.horizontal, 20.adjustedWidth)
+                .padding(.bottom, 16.adjustedHeight)
+            
+            ForEach(places.prefix(3), id: \.placeId) { place in
+                SearchPlaceCard(
+                    thumbnailUrl: place.thumbnailImageUrl,
+                    placeName: place.placeName,
+                    address: place.address,
+                    mainTag: place.primaryTag,
+                )
                 .onTapGesture {
-                    registerAction?()
+                    placeDetailAction?(place.townId, place.placeId)
                 }
+            }
+            
+            HStack(alignment: .center, spacing: 0) {
+                Text("찾는 장소가 없어요")
+                    .applySolplyFont(.button_14_m)
+                    .foregroundColor(.gray700)
+                    .padding(.leading, 20.adjustedWidth)
+                
+                Image(.arrowRightIconGray)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 24.adjustedWidth, height: 10.adjustedHeight)
+            }
+            .padding(.top, 16.adjustedHeight)
+            .onTapGesture {
+                registerAction?()
             }
         }
     }
