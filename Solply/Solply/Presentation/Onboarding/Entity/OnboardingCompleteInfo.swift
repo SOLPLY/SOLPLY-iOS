734270f09af2fc47782fd881e93b9d759c1f314a
dudwntjs <sts07190@naver.com>
2025-11-03 21:54:19 +0900

    [feat/#320] policyAgreementInfos 필드 추가

--- Solply/Solply/Presentation/Onboarding/Entity/OnboardingCompleteInfo.swift
+++ Solply/Solply/Presentation/Onboarding/Entity/OnboardingCompleteInfo.swift
@@ -5,9 +5,32 @@
 //  Created by 선영주 on 7/17/25.
 //
 
-struct OnboardingCompleteInfo {
+import Foundation
+
+struct OnboardingCompleteInfo: Encodable {
+    let policyAgreementInfos: [PolicyAgreementInfo]
     let townId: Int
     let townName: String
     let persona: String
     let nickname: String
+
+    init(
+        townId: Int,
+        townName: String,
+        persona: String,
+        nickname: String,
+        policyAgreementInfos: [PolicyAgreementInfo] = []
+    ) {
+        self.townId = townId
+        self.townName = townName
+        self.persona = persona
+        self.nickname = nickname
+        self.policyAgreementInfos = policyAgreementInfos
+    }
 }
+
+struct PolicyAgreementInfo: Encodable {
+    let policyId: Int
+    let isAgree: Bool
+}
+
