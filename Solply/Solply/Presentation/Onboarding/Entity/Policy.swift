16d56a399798a38981f9f4af12c894dbea1393ab
dudwntjs <sts07190@naver.com>
2025-11-03 21:44:19 +0900

    [feat/#320] Policy Entity 구현

---
+++ Solply/Solply/Presentation/Onboarding/Entity/Policy.swift
@@ -0,0 +1,14 @@
+//
+//  Policy.swift
+//  Solply
+//
+//  Created by sun on 11/3/25.
+//
+
+struct Policy {
+    let id: Int
+    let title: String
+    let content: String
+    let isRequired: Bool
+    var isAgreed: Bool = false
+}
