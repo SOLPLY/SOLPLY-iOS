496a897fd62a6ff9e05bd734df578eb50fb5679b
Won <andrew112517@naver.com>
2025-11-14 16:00:00 +0900

    [fix/#332] UserInformationResponseDTO 수정

--- Solply/Solply/Network/DTO/User/ResponseDTO/UserInformationResponseDTO.swift
+++ Solply/Solply/Network/DTO/User/ResponseDTO/UserInformationResponseDTO.swift
@@ -10,11 +10,19 @@ import Foundation
 struct UserInformationResponseDTO: ResponseModelType {
     let userId: Int
     let nickname: String
+    let profileImageUrl: String?
     let selectedTown: SelectedTownDTO
     let persona: PersonaType
+    let myPlacePreviews: [PlacePreviewResponseDTO]
 }
 
-struct SelectedTownDTO: ResponseModelType{
+struct SelectedTownDTO: ResponseModelType {
     let townId: Int
     let townName: String
+}
+
+struct PlacePreviewResponseDTO: ResponseModelType {
+    let placeId: Int
+    let placeName: String
+    let thumbnailImageUrl: String
 }
