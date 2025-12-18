86b0f0df57a207a78da3b079bad3ff653e83e3e7
Won <andrew112517@naver.com>
2025-12-19 02:34:17 +0900

    [feat/#363] filterButton 서브태그 선택 UI 구현

--- Solply/Solply/Presentation/Place/Component/FilterButton.swift
+++ Solply/Solply/Presentation/Place/Component/FilterButton.swift
@@ -12,12 +12,18 @@ struct FilterButton: View {
     // MARK: - Properties
     
     private let title: String
+    private let selectedSubTagsCount: Int
     private let action: (() -> Void)?
     
     // MARK: - Initializer
     
-    init(title: String, action: (() -> Void)? = nil) {
+    init(
+        title: String,
+        selectedSubTagsCount: Int = 0,
+        action: (() -> Void)? = nil
+    ) {
         self.title = title
+        self.selectedSubTagsCount = selectedSubTagsCount
         self.action = action
     }
     
@@ -28,20 +34,34 @@ struct FilterButton: View {
             action?()
         } label: {
             HStack(alignment: .center, spacing: 0) {
-                Text(title)
-                    .applySolplyFont(.body_14_m)
+                HStack(alignment: .center, spacing: 4.adjustedWidth) {
+                    Text(title)
+                        .applySolplyFont(selectedSubTagsCount == 0 ? .body_14_r : .body_14_m)
+                        .foregroundStyle(.coreBlack)
+                    
+                    if selectedSubTagsCount > 1 {
+                        Text("외 \(selectedSubTagsCount - 1)개")
+                            .applySolplyFont(.body_14_r)
+                            .foregroundStyle(.coreBlack)
+                    }
+                }
                 Image(.arrowDownIcon)
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(width: 24.adjusted, height: 24.adjusted)
             }
             .foregroundStyle(.coreBlack)
-            .background(.coreWhite)
             .padding(.vertical, 5.5.adjustedHeight)
             .padding(.leading, 12.adjustedWidth)
             .padding(.trailing, 4.adjustedWidth)
+            .background(selectedSubTagsCount == 0 ? .coreWhite : .gray100)
             .capsuleClipped()
-            .addBorder(.capsule, borderColor: .gray300, borderWidth: 0.75)
+            .addBorder(
+                .capsule,
+                borderColor: .gray300,
+                borderWidth: selectedSubTagsCount == 0 ? 0.75 : 1
+            )
         }
+        .buttonStyle(.plain)
     }
 }
