d80168ccd9b4e009d65ff37d924e6b1b19a049f5
Won <andrew112517@naver.com>
2026-01-12 17:36:31 +0900

    [feat/#384] authenticationRequired alert case 추가

--- Solply/Solply/Global/Enum/AlertType.swift
+++ Solply/Solply/Global/Enum/AlertType.swift
@@ -14,22 +14,23 @@ enum AlertType {
     case photoPermissionDenied
     case logout
     case withdraw
+    case authenticationRequired
     
     var title: String {
         switch self {
         case .deletePlace: return "선택한 장소를 삭제할까요?"
         case .deleteCourse: return "선택한 코스를 삭제할까요?"
         case .changesNotSaved: return "변경 사항을 저장하지 않고\n나가시겠어요?"
-        case .photoPermissionDenied: return "앱에서 사진을 사용하려면 권한을 허용해주세요"
+        case .photoPermissionDenied: return "앱에서 사진을 사용하려면 권한을 허용해주세요."
         case .logout: return "로그아웃하시겠습니까?"
         case .withdraw: return "탈퇴하시겠습니까?"
+        case .authenticationRequired: return "로그인이 필요한 기능이에요."
         }
     }
     
     var cancelText: String {
         switch self {
-        case .deletePlace, .deleteCourse, .changesNotSaved, .photoPermissionDenied, .logout, .withdraw:
-            return "취소"
+        default: return "취소"
         }
     }
     
@@ -45,6 +46,8 @@ enum AlertType {
             return "로그아웃"
         case .withdraw:
             return "탈퇴"
+        case .authenticationRequired:
+            return "로그인"
         }
     }
 }
