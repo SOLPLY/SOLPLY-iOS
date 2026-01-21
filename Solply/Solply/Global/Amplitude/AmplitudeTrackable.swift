9a81c6bb10746102cc7fc9a5b10ee1a8c30d7a0b
Won <andrew112517@naver.com>
2026-01-21 15:11:33 +0900

    [feat/#388] AmplitudeTrackable protocol 추가

---
+++ Solply/Solply/Global/Amplitude/AmplitudeTrackable.swift
@@ -0,0 +1,13 @@
+//
+//  AmplitudeTrackable.swift
+//  Solply
+//
+//  Created by 김승원 on 1/21/26.
+//
+
+import Foundation
+
+protocol AmplitudeTrackable {
+    var name: String { get }
+    var properties: [String: Any] { get }
+}
