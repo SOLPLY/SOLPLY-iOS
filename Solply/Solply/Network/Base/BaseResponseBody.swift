babf8aa7d2ee42785e4ac6be251b667e61d4ed1c
Won <andrew112517@naver.com>
2026-02-15 20:25:08 +0900

    [chore/#389] CourseService Mock데이터 추가

--- Solply/Solply/Network/Base/BaseResponseBody.swift
+++ Solply/Solply/Network/Base/BaseResponseBody.swift
@@ -14,3 +14,19 @@ struct BaseResponseBody<T: ResponseModelType>: ResponseModelType {
     let message: String
     let data: T?
 }
+
+/// mockData용 응답
+extension BaseResponseBody {
+    static func mockSuccess(
+        data: T,
+        message: String = "mock 데이터를 불러오는 데 성공했습니다."
+    ) -> BaseResponseBody<T> {
+
+        return BaseResponseBody(
+            success: true,
+            code: "200",
+            message: message,
+            data: data
+        )
+    }
+}
