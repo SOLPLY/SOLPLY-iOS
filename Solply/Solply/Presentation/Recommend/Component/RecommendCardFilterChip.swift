c79be65981705b7296b4addfbeee5763e4403052
sun <sts07190@naver.com>
2026-03-12 09:12:52 +0900

    [feat/#429] RecommeneCardFilterChip 구현

---
+++ Solply/Solply/Presentation/Recommend/Component/RecommendCardFilterChip.swift
@@ -0,0 +1,28 @@
+//
+//  RecommendCardFilterChip.swift
+//  Solply
+//
+//  Created by sun on 3/12/26.
+//
+
+import SwiftUI
+
+struct RecommendCardFilterChip: View {
+    
+    let title: String
+    
+    var body: some View {
+        HStack(alignment: .center, spacing: 4.adjustedWidth) {
+            Text(title)
+                .applySolplyFont(.body_14_m)
+                .foregroundColor(.gray900)
+        }
+        .padding(.horizontal, 12.adjustedWidth)
+        .padding(.vertical, 4.adjustedHeight)
+        .frame(height: 32.adjustedWidth, alignment: .center)
+        .background(
+            Capsule()
+                .fill(.gray100)
+        )
+    }
+}
