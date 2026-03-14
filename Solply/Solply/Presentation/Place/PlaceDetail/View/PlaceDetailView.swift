c05a1999b02b125d34765197e960273aadb9b2a2
Won <andrew112517@naver.com>
2026-03-14 18:58:09 +0900

    [feat/#428] RecordListView 연결

--- Solply/Solply/Presentation/Place/PlaceDetail/View/PlaceDetailView.swift
+++ Solply/Solply/Presentation/Place/PlaceDetail/View/PlaceDetailView.swift
@@ -341,7 +341,7 @@ extension PlaceDetailView {
     private var record: some View {
         VStack(alignment: .center, spacing: 20.adjustedHeight) {
             sectionHeader(title: "기록", moreButtonAction: store.state.records.isEmpty ? nil : {
-                // TODO: - 기록 더보기 뷰 넘기기
+                appCoordinator.navigate(to: .recordList)
             })
             .padding(.horizontal, 20.adjustedWidth)
             
