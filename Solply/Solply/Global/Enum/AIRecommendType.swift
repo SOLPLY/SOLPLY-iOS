140c466ba03242afbb4cec820c2d7b90c82f9d53
sun <sts07190@naver.com>
2026-03-15 11:27:31 +0900

    [feat/#440] AIRecommendType Enum 추가

---
+++ Solply/Solply/Global/Enum/AIRecommendType.swift
@@ -0,0 +1,33 @@
+//
+//  AIRecommendType.swift
+//  Solply
+//
+//  Created by sun on 3/15/26.
+//
+
+import SwiftUI
+
+enum AIRecommendType {
+    case main(MainTagType)
+    case course(CourseTagType)
+    
+    var icon: ImageResource? {
+        switch self {
+        case .main(let tag):
+            return tag.aiIcon
+        case .course:
+            return nil
+        }
+    }
+    
+    // MARK: - Properties
+    
+    var titleColor: Color {
+        switch self {
+        case .main(let tag):
+            return tag.titleColor ?? .coreBlack
+        case .course(let tag):
+            return tag.titleColor
+        }
+    }
+}
