866b16dc09c23f086de604d624251e06ed20bdfa
seozero <tjwndud0412@naver.com>
2026-03-16 13:33:11 +0100

    [feat/#447] prompt placeholder enum 정의

--- Solply/Solply/Global/Enum/SolplyContentType.swift
+++ Solply/Solply/Global/Enum/SolplyContentType.swift
@@ -46,4 +46,13 @@ extension SolplyContentType {
             ]
         }
     }
+    
+    var aiRecommendPromptPlaceholder: String {
+        switch self {
+        case .place:
+            return "작업하기 좋은 우드톤 카페\n시간 제한 없는 조용한 디저트 카페\n혼자 생각 정리하기 좋은 산책로"
+        case .course:
+            return "소품샵 중심 구경하기 좋은 코스\n평일 오후에 가기 좋은 반나절 코스 "
+        }
+    }
 }
