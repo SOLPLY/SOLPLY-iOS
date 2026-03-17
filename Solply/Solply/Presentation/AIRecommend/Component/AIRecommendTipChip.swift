868c7b777254cb166ac727aa38991ca9fffbbdb5
sun <sts07190@naver.com>
2026-03-17 09:20:04 +0900

    [fix/#440] Initializer 추가

--- Solply/Solply/Presentation/AIRecommend/Component/AIRecommendTipChip.swift
+++ Solply/Solply/Presentation/AIRecommend/Component/AIRecommendTipChip.swift
@@ -9,8 +9,22 @@ import SwiftUI
 
 struct AIRecommendTipChip: View {
     
+    // MARK: - Properties
+    
     let text: String
     let tag: AIRecommendType
+    
+    // MARK: - Initializer
+    
+    init(
+        text: String,
+        tag: AIRecommendType
+    ) {
+        self.text = text
+        self.tag = tag
+    }
+    
+    // MARK: - Body
     
     var body: some View {
         HStack(alignment: .top, spacing: 4.adjustedWidth) {
