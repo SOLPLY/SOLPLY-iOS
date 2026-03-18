8859d7bf32859ff94aa727c399fe230169db0015
sun <sts07190@naver.com>
2026-03-18 09:01:10 +0900

    [refactor/#453] PromptText 컴포넌트 추가

--- Solply/Solply/Presentation/AIRecommend/AIRecommendResult/View/AIRecommendResultView.swift
+++ Solply/Solply/Presentation/AIRecommend/AIRecommendResult/View/AIRecommendResultView.swift
@@ -39,26 +39,10 @@ struct AIRecommendResultView: View {
 // MARK: - UI Components
 
 private extension AIRecommendResultView {
-    
     var promptText: some View {
-        ZStack(alignment: .leading) {
-            RoundedRectangle(cornerRadius: 20)
-                .fill(Color.gray100)
-                .frame(height: 52.adjustedHeight)
-                .addBorder(
-                    .roundedRectangle(cornerRadius: 20),
-                    borderColor: .gray300,
-                    borderWidth: 1
-                )
-            
-            HStack(alignment: .center, spacing: 0) {
-                Text("서버에서 값 가져오는 거겠지")
-                    .applySolplyFont(.body_14_r)
-                    .foregroundColor(.gray900)
-            }
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .padding(.horizontal, 20.adjustedWidth)
-        }
+        AIRecommendPromptText(
+            prompt: "서버에서 값 가져오는 거겠지"
+        )
         .padding(.bottom, 16.adjustedHeight)
         .padding(.horizontal, 16.adjustedWidth)
     }
