4f4a1322a0b80ff45a7caba726d1411d17c340b5
seozero <tjwndud0412@naver.com>
2026-03-20 17:09:37 +0100

    [feat/#466] 동네 선택 바텀 시트 api 연결

--- Solply/Solply/Presentation/AIRecommend/AIRecommendPrompt/State/AIRecommendPromptReducer.swift
+++ Solply/Solply/Presentation/AIRecommend/AIRecommendPrompt/State/AIRecommendPromptReducer.swift
@@ -21,6 +21,67 @@ enum AIRecommendPromptReducer {
             
         case .updatePromptText(let text):
             state.isRecommendButtonEnabled = text.count >= 5
+            
+        case .showTownSelectBottomSheet(let isSheetPresented):
+            state.isTownSelectBottomSheetPresented = isSheetPresented
+            
+        // api
+            
+        case .fetchTowns:
+            state.isTownLoading = true
+            
+        case .fetchTownsSuccess(let townList):
+            state.townList = townList
+            
+            let subTowns = townList.flatMap { $0.subTowns }
+            state.selectedSubTown = subTowns.first { $0.id == state.initialTownId }
+            state.currentSelectedSubTown = state.selectedSubTown
+            
+            if let selectedSubTown = state.selectedSubTown {
+                state.selectedTown = townList.first { $0.subTowns.contains(selectedSubTown) }
+            } else {
+                if let firstTown = townList.first {
+                    state.selectedTown = firstTown
+                    state.selectedSubTown = firstTown.subTowns.first
+                    state.currentSelectedSubTown = state.selectedSubTown
+                } else {
+                    state.selectedTown = nil
+                    state.selectedSubTown = nil
+                    state.currentSelectedSubTown = nil
+                }
+            }
+            
+            state.isTownLoading = false
+            
+        case .fetchTownsFailure(let error):
+            print(error)
+            
+        case .setInitialTownId(let townId):
+            state.initialTownId = townId
+            
+        case .selectTown(let town):
+            state.selectedTown = town
+            
+            let subTowns = town.subTowns
+            
+            if let current = state.currentSelectedSubTown, subTowns.contains(current) {
+                state.selectedSubTown = current
+            } else {
+                state.selectedSubTown = subTowns.first
+            }
+            
+        case .selectSubTown(let subTown):
+            state.selectedSubTown = subTown
+            state.currentSelectedSubTown = subTown
+            
+            
+        // TODO: - API에 따라 달라질듯
+        case .saveSelection:
+            break
+        case .saveSelectionSuccess:
+            break
+        case .saveSelectionFailure(let error):
+            break
         }
     }
 }
