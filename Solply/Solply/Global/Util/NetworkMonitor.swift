13cb1b34da3cba6aef12ff9f184a3ad71f6e6fc6
Won <andrew112517@naver.com>
2026-03-31 01:49:01 +0900

    [feat/#475] 네트워크 모니터 수정

--- Solply/Solply/Global/Util/NetworkMonitor.swift
+++ Solply/Solply/Global/Util/NetworkMonitor.swift
@@ -13,24 +13,39 @@ final class NetworkMonitor {
     
     // MARK: - Properties
     
-    private let networkMonitor = NWPathMonitor()
+    private let monitor = NWPathMonitor()
     private let queue = DispatchQueue(label: "NetworkMonitor")
-    private(set) var isConnected: Bool = false
     
+    private(set) var isConnected: Bool = true
     
     // MARK: - Initializer
     
     init() {
-        networkMonitor.pathUpdateHandler = { [weak self] path in
+        monitor.pathUpdateHandler = { [weak self] path in
             guard let self else { return }
             
-            self.isConnected = path.status == .satisfied
+            DispatchQueue.main.async {
+                self.isConnected = path.status == .satisfied
+            }
         }
         
-        networkMonitor.start(queue: queue)
+        monitor.start(queue: queue)
+    }
+    
+    func checkConnection() -> Bool {
+        monitor.currentPath.status == .satisfied
     }
     
     deinit {
-        networkMonitor.cancel()
+        monitor.cancel()
+    }
+}
+
+@Observable
+final class RetryRegistry {
+    private(set) var reloadID = UUID()
+    
+    func reload() {
+        reloadID = UUID()
     }
 }
