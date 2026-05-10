5a551166cc17f64b89f58ea7809f3461cea977da
Won <andrew112517@naver.com>
2026-05-11 03:40:51 +0900

    [feat/#509] ai추천 결과 townId 넘기게 수정

--- Solply/Solply/Presentation/AIRecommend/AIRecommendResult/Entity/AIRecommendCard.swift
+++ Solply/Solply/Presentation/AIRecommend/AIRecommendResult/Entity/AIRecommendCard.swift
@@ -25,6 +25,7 @@ struct AIRecommendPlaceCardItem: Equatable, Hashable {
     let id: Int
     let mainTag: MainTagType
     let placeName: String
+    let townId: Int
     let townName: String
     let tipText: String
     let filters: [String]
@@ -40,53 +41,9 @@ struct AIRecommendCourseCardItem: Equatable, Hashable {
     let id: Int
     let courseTagType: CourseTagType
     let courseName: String
+    let townId: Int
     let townName: String
     let tipText: String
     let courseCounts: [AIRecommendCourseCountItem]
     let thumbnailImageUrl: String?
-}
-
-// TODO: - API 연결하면 지울게욥!!
-
-extension AIRecommendCard {
-    static let mockData: [AIRecommendCard] = [
-        .place(
-            AIRecommendPlaceCardItem(
-                id: 1,
-                mainTag: .cafe,
-                placeName: "카페",
-                townName: "포항",
-                tipText: "조용한 분위기에서 혼자 작업하기 좋아요",
-                filters: ["조용함", "콘센트", "와이파이"],
-                thumbnailImageUrl: nil
-            )
-        ),
-        .course(
-            AIRecommendCourseCardItem(
-                id: 2,
-                courseTagType: .healing,
-                courseName: "힐링이다",
-                townName: "포항항",
-                tipText: "산책하고 커피 마시기 좋은 코스예요",
-                courseCounts: [
-                    AIRecommendCourseCountItem(mainTag: "카페", count: 2),
-                    AIRecommendCourseCountItem(mainTag: "산책", count: 1),
-                    AIRecommendCourseCountItem(mainTag: "음식", count: 1),
-                    AIRecommendCourseCountItem(mainTag: "서점/책방", count: 1)
-                ],
-                thumbnailImageUrl: nil
-            )
-        ),
-        .place(
-            AIRecommendPlaceCardItem(
-                id: 3,
-                mainTag: .food,
-                placeName: "맛도리",
-                townName: "포항항항",
-                tipText: "혼밥하기 편하고 분위기가 아늑해요",
-                filters: ["혼밥", "브런치", "분위기"],
-                thumbnailImageUrl: nil
-            )
-        )
-    ]
 }
--- Solply/Solply/Presentation/AIRecommend/AIRecommendResult/View/AIRecommendResultView.swift
+++ Solply/Solply/Presentation/AIRecommend/AIRecommendResult/View/AIRecommendResultView.swift
@@ -83,8 +83,7 @@ private extension AIRecommendResultView {
                         thumbnailImageUrl: item.thumbnailImageUrl
                     )
                     .onTapGesture {
-                        // TODO: 장소 카드 탭 시 장소 상세 (townId 수정 필요)
-                        appCoordinator.navigate(to: .placeDetail(townId: 1, placeId: item.id, fromSearch: false))
+                        appCoordinator.navigate(to: .placeDetail(townId: item.townId, placeId: item.id, fromSearch: true))
                     }
 
                 case .course(let item):
@@ -97,8 +96,7 @@ private extension AIRecommendResultView {
                         thumbnailImageUrl: item.thumbnailImageUrl
                     )
                     .onTapGesture {
-                        // TODO: 코스 카드 탭 시 코스 상세 (townId 수정 필요)
-                        appCoordinator.navigate(to: .courseDetail(townId: 1, courseId: item.id, fromArchive: false))
+                        appCoordinator.navigate(to: .courseDetail(townId: item.townId, courseId: item.id, fromArchive: false))
                     }
                 }
             }
